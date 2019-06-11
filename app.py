e840f5c4ae68037855c77fb79248a67b3257726d
hisham waleed karam <hisham.karam@cartologic.com>
2019-06-11 16:23:45 +0200

    cql_filter

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import json
+# import json
 import os
 import dash
 import dash_core_components as dcc
@@ -12,7 +12,7 @@ from requests.utils import unquote
 
 current_dir = os.path.dirname(__file__)
 GEOSERVER_LOCATION = os.getenv(
-    "GEOSERVER_LOCATION", "http://localhost:8080/geoserver/"
+    "GEOSERVER_LOCATION", "http://localhost/geoserver14-geonode/"
 )
 WFS_URL = urljoin(GEOSERVER_LOCATION, "wfs")
 WMS_URL = urljoin(GEOSERVER_LOCATION, "wms")
@@ -22,7 +22,7 @@ MAPBOX_ACCESS_TOKEN = os.getenv(
 )
 
 
-def build_wms_layer_url(base_url, layername):
+def build_wms_layer_url(base_url, layername, CQL_FILTER=None):
     params = {
         "SERVICE": "WMS",
         "VERSION": "1.1.1",
@@ -35,6 +35,8 @@ def build_wms_layer_url(base_url, layername):
         "WIDTH": 256,
         "HEIGHT": 256,
     }
+    if CQL_FILTER:
+        params.update({"cql_Filter": CQL_FILTER})
     req = PreparedRequest()
     req.prepare_url(base_url, params)
     return req.url
@@ -62,35 +64,13 @@ def get_filtered_data_frame(layername="geonode:cpi_layer", col=None, value=None)
     return df
 
 
-def get_map_graph_layout(title="CPI Choropleth Map"):
-    return go.Layout(
-        title=go.layout.Title(text=title),
-        geo=go.layout.Geo(
-            scope="world",
-            projection=go.layout.geo.Projection(type="mercator", scale=3.5),
-            showlakes=True,
-            showland=True,
-            center={"lon": 23.992767, "lat": 5.088892},
-        ),
-    )
-
-
-def get_map_choropleth(locations, z, locationmode="ISO-3", title="CPI"):
-    return go.Choropleth(
-        autocolorscale=True,
-        locations=locations,
-        z=z,
-        locationmode=locationmode,
-        marker=go.choropleth.Marker(
-            line=go.choropleth.marker.Line(color="rgb(255,255,255)", width=2)
-        ),
-        colorbar=go.choropleth.ColorBar(title=title),
-    )
-
-
-def get_bar_trace(df, x_col, y_col, name="CPI"):
+def get_bar_trace(df, x_col, y_col, name="CPI", orientation="v"):
     trace = go.Bar(
-        x=df[x_col], y=df[y_col], name=name, marker=dict(color="rgb(49,130,189)")
+        x=df[x_col],
+        y=df[y_col],
+        name=name,
+        marker=dict(color="rgb(49,130,189)"),
+        orientation=orientation,
     )
     return trace
 
@@ -136,11 +116,28 @@ app.layout = html.Div(
         html.Br(),
         html.Div(
             className="charts-container",
-            children=[dcc.Graph(id="mapDiv"), dcc.Graph(id="bar-chart")],
+            children=[dcc.Graph(id="bar-chart"), dcc.Graph(id="bar-chart2")],
         ),
         dcc.Graph(id="mapbox-map"),
     ],
 )
+
+
+@app.callback(
+    Output("bar-chart2", "figure"),
+    [Input("bar-chart", "clickData"), Input("layer-selector", "value")],
+)
+def country_years_chart(selected_data, selected_layer):
+    title = INDICATORS[selected_layer]["title"]
+    if selected_data:
+        country = selected_data["points"][0]["y"]
+        df = get_filtered_data_frame(selected_layer, "iso3", country)
+        data = get_bar_trace(df, "data_year", "data_value", title)
+        layout = get_bar_trace_layout("Year", "Value", title=country)
+        figure_data = dict(data=[data], layout=layout)
+        return go.Figure(figure_data)
+    layout = get_bar_trace_layout("Year", "Value", title="No Country Selected")
+    return go.Figure(dict(data=[], layout=layout))
 
 
 @app.callback(Output("year_slider", "min"), [Input("layer-selector", "value")])
@@ -166,19 +163,6 @@ def layer_inidicator_marks(indicator):
 
 
 @app.callback(
-    Output("mapDiv", "figure"),
-    [Input("year_slider", "value"), Input("layer-selector", "value")],
-)
-def time_series_figure(selected_year, selected_layer):
-    title = INDICATORS[selected_layer]["title"]
-    dff = get_filtered_data_frame(selected_layer, col="data_year", value=selected_year)
-    figure_data = [
-        get_map_choropleth(dff["iso3"], dff["data_value"].astype(float), title=title)
-    ]
-    return go.Figure(data=figure_data, layout=get_map_graph_layout(title=title))
-
-
-@app.callback(
     Output("bar-chart", "figure"),
     [Input("year_slider", "value"), Input("layer-selector", "value")],
 )
@@ -186,22 +170,33 @@ def time_series_bar_chart(selected_year, selected_layer):
     title = INDICATORS[selected_layer]["title"]
     dff = get_filtered_data_frame(selected_layer, col="data_year", value=selected_year)
     figure_data = dict(
-        data=[get_bar_trace(dff, "iso3", "data_value", name=title)],
-        layout=get_bar_trace_layout("Year", "CPI", title=title),
+        data=[get_bar_trace(dff, "data_value", "iso3", name=title, orientation="h")],
+        layout=get_bar_trace_layout("Value", "Country", title=title),
     )
     return go.Figure(figure_data)
 
 
 @app.callback(
     Output("mapbox-map", "figure"),
-    [Input("year_slider", "value"), Input("layer-selector", "value")],
+    [
+        Input("year_slider", "value"),
+        Input("layer-selector", "value"),
+        Input("bar-chart", "clickData"),
+    ],
 )
-def mapbox_map_time_series(selected_year, selected_layer):
+def mapbox_map_time_series(selected_year, selected_layer, selected_data):
+    country = None
+    if selected_data:
+        country = selected_data["points"][0]["y"]
 
-    dff = get_filtered_data_frame(selected_layer, col="data_year", value=selected_year)
-    dff_json = dff.to_json()
-    dff_geojson = json.loads(dff_json)
+    # dff = get_filtered_data_frame(selected_layer, col="data_year", value=selected_year)
+    # dff_json = dff.to_json()
+    # dff_geojson = json.loads(dff_json)
     map_data = [go.Scattermapbox(lat=["24.667298"], lon=["25.664063"], mode="markers")]
+    if country:
+        wms_filter = "data_year = '{}' and iso3 = '{}'".format(selected_year, country)
+    else:
+        wms_filter = "data_year = '{}'".format(selected_year)
     map_layout = go.Layout(
         height=600,
         autosize=True,
@@ -210,7 +205,11 @@ def mapbox_map_time_series(selected_year, selected_layer):
             layers=[
                 dict(
                     sourcetype="raster",
-                    source=[build_wms_layer_url(WMS_URL, selected_layer)],
+                    source=[
+                        build_wms_layer_url(
+                            WMS_URL, selected_layer, CQL_FILTER=wms_filter
+                        )
+                    ],
                     type="raster",
                     visible=True,
                 )
